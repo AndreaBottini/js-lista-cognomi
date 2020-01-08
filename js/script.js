01b13cc4bffcb6b39d0552c3793f4a4bb9ea8ff4
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-08 14:47:42 +0100

    Inizio e impostazione esercizio

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// alert("funziona?")
+
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi, stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
