a11ad700cf56b39599ade7f157967c7fdd170f56
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-08 16:18:15 +0100

    Update e termine esercizio

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@
 // Chiedi all’utente il cognome,
 // inseriscilo in un array con altri cognomi, stampa la lista ordinata alfabeticamente.
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova.
+
+
+//Chiedi all'utente il cognome
+var cognomePersona = prompt ("Inserisci il tuo cognome all'interno del box");
+// console.log(cognomePersona);
+
+// inseriscilo in un array con altri cognomi
+var variCognomi = ["Alberti", "Rossi", "Grandi", "Baglioni", "Arrighi", "Rossi", "Genovesi", "Zoncoli", "Sampietri"];
+// console.log(variCognomi);
+variCognomi.push(cognomePersona);
+
+//stampa la lista ordinata alfabeticaente.
+variCognomi.sort()
+console.log(variCognomi)
+
+
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
+for (var i = 0; i < variCognomi.length; i++) {
+  document.getElementById('lista_cognomi').innerHTML = 
+  document.getElementById('lista_cognomi').innerHTML + "<li>" + variCognomi[i] + " " + (i+1) + "</li>"
+}
+
+document.getElementById('lista_cognomi').classList.add("list_style_none");
