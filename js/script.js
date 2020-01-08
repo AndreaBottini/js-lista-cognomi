2b904ff708fb868b9e8186b2f7f3972be0a7dd41
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-08 17:18:29 +0100

    Correzione posizione Utente

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var cognomePersona = prompt ("Inserisci il tuo cognome all'interno del box");
 // console.log(cognomePersona);
 
 // inseriscilo in un array con altri cognomi
-var variCognomi = ["Alberti", "Rossi", "Grandi", "Baglioni", "Arrighi", "Rossi", "Genovesi", "Zoncoli", "Sampietri"];
+var variCognomi = ["Alberti", "Rossi", "Grandi", "Baglioni", "Arrighi", "Camilli", "Genovesi", "Zoncoli", "Sampietri"];
 // console.log(variCognomi);
 variCognomi.push(cognomePersona);
 
@@ -21,8 +21,12 @@ console.log(variCognomi)
 
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova.
 for (var i = 0; i < variCognomi.length; i++) {
-  document.getElementById('lista_cognomi').innerHTML = 
-  document.getElementById('lista_cognomi').innerHTML + "<li>" + variCognomi[i] + " " + (i+1) + "</li>"
+  document.getElementById('lista_cognomi').innerHTML =
+  document.getElementById('lista_cognomi').innerHTML + "<li>" + variCognomi[i] + "</li>"
 }
 
+var numeroPosizione = (variCognomi.indexOf(cognomePersona) + 1);
+document.getElementById('posizione').innerHTML = numeroPosizione;
+
+//Eliminazione punti elenco
 document.getElementById('lista_cognomi').classList.add("list_style_none");
